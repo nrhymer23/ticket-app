bf7ed37ecb77ca9f2c1d1ea6801421d9cf297a5d
nrhymer23 <nrhymer23@gmail.com>
2024-01-30 19:50:54 -0500

    Form Creation Completed

--- app/(components)/TicketForm.js
+++ app/(components)/TicketForm.js
@@ -4,6 +4,20 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const TicketForm = () => {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    console.log();
+  };
+
   const startingTicketData = {
     title: "",
     description: "",
@@ -15,7 +29,11 @@ const TicketForm = () => {
   const [formData, setFormData] = useState(startingTicketData);
   return (
     <div className="flex justify-center">
-      <form>
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <h3>Create Your Ticket</h3>
         <label>Title</label>
         <input
@@ -26,6 +44,94 @@ const TicketForm = () => {
           required={true}
           value={formData.title}
         />
+
+        <label>Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows="5"
+        />
+
+        <label>Category</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Hardware Problem">Hardware Problem</option>
+          <option value="Software Bug">Software Bug</option>
+          <option value="Website Problem">Website Problem</option>
+          <option value="Other Issue">Other Issue</option>
+        </select>
+
+        <label>Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+        <label>Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress}
+          min="0"
+          max="100"
+          onChange={handleChange}
+        />
+        <label>Status</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="not started">--Not Started--</option>
+          <option value="started">--Started--</option>
+          <option value="completed">--Completed--</option>
+        </select>
+        <input type="submit" className="btn" value="Create Ticket" />
       </form>
     </div>
   );
